185e4f3fd9d79635791cbcefa6cd422df19d5f8e
Leonard Box <lfbox7@yahoo.com>
2022-02-21 16:36:34 -0600

    add title class on app class

--- src/com/guessaword/model/board/TitlePanel.java
+++ src/com/guessaword/model/board/TitlePanel.java
@@ -1,9 +1,37 @@
 package com.guessaword.model.board;
 
 public class TitlePanel {
+    public static final String BOLD = "\u001B[1m";
+    public static final String ITALIC = "\u001B[3m";
+    public static final String UNDERLINE = "\u001B[4m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED	= "\u001B[31m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_GRAY = "\u001B[37m";
 
     public void showTitle() {
 
+        title();
+        showInstructions();
+    }
 
+    public void title() {
+        System.out.println(ANSI_CYAN + "███████╗ ██╗   ██╗███████╗███████╗███████╗     █████╗     ██╗    ██╗ ██████╗ ██████╗ ██████╗\n" +
+             "██╔════╝ ██║   ██║██╔════╝██╔════╝██╔════╝    ██╔══██╗    ██║    ██║██╔═══██╗██╔══██╗██╔══██╗\n" +
+             "██║  ███╗██║   ██║█████╗  ███████╗███████╗    ███████║    ██║ █╗ ██║██║   ██║██████╔╝██║  ██║\n" +
+             "██║   ██║██║   ██║██╔══╝  ╚════██║╚════██║    ██╔══██║    ██║███╗██║██║   ██║██╔══██╗██║  ██║\n" +
+             "╚██████╔╝╚██████╔╝███████╗███████║███████║    ██║  ██║    ╚███╔███╔╝╚██████╔╝██║  ██║██████╔╝\n" +
+             " ╚═════╝  ╚═════╝ ╚══════╝╚══════╝╚══════╝    ╚═╝  ╚═╝     ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═════╝\n\n" + ANSI_RESET);
+    }
+
+    public void showInstructions() {
+        System.out.println(UNDERLINE + BOLD + ANSI_CYAN + "INSTRUCTIONS" + ANSI_RESET);
+        System.out.println(ITALIC + ANSI_RED + "Guess a Word has chosen a 5-letter word. You have 6 tries to guess the word. ");
+        System.out.println("Correct and incorrect letters in each of your guesses will be identified as follows:" + ANSI_RESET);
+        System.out.println("- Letters highlighted in " + UNDERLINE + BOLD + ANSI_GREEN + "G R E E N" + ANSI_RESET + " appear in the word and are in the correct position.");
+        System.out.println("- Letters highlighted in " + UNDERLINE + BOLD + ANSI_YELLOW + "Y E L L O W" + ANSI_RESET + " appear in the word but in a different position.");
+        System.out.println("- Letters highlighted in " + UNDERLINE + BOLD + ANSI_GRAY + "G R A Y" + ANSI_RESET + " do not appear in the word.\n\n");
     }
 }
--- src/com/guessaword/model/board/UserPanel.java
+++ src/com/guessaword/model/board/UserPanel.java
@@ -2,38 +2,50 @@ package com.guessaword.model.board;
 
 import com.guessaword.model.Player;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class UserPanel {
-
+    private static final String LIST_OF_PLAYERS_FROM_FILE = "Dictionary/players.dat";
     Player player;
-    List<Player> players;
+    List<String> players;
 
     public String showUser(String username) {
         players = getListOfPlayers();
         if (!checkIfValidUser(username)) {
-            // create a new user
-            // player = new Player(username);
+            //player = new Player(username);
         } else {
             player = getPlayerFromList(username);
         }
-        String user = "";
-        // to display username and statistics
-        return user;
+        return player.toString();
     }
 
-    private List<Player> getListOfPlayers() {
-        // read list of players from file
+    private List<String> getListOfPlayers() {
+        try {
+            players = Files.readAllLines(Path.of(LIST_OF_PLAYERS_FROM_FILE));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return players;
     }
 
     private boolean checkIfValidUser(String username) {
-        // check if username is on list
-        return true;
+        for (String playah : players) {
+            if (playah.contains(username)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private Player getPlayerFromList(String username) {
-        // get player object from list
+        for (String playah : players) {
+            if (playah.contains(username)) {
+                //player = new Player(username);
+            }
+        }
         return player;
     }
 }

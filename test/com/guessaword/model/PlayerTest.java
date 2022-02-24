b397a41e65011ffb1d0735d7b110a073e078730b
Leonard Box <lfbox7@yahoo.com>
2022-02-24 13:06:55 -0600

    test updates

--- test/com/guessaword/model/PlayerTest.java
+++ test/com/guessaword/model/PlayerTest.java
@@ -1,12 +1,41 @@
 package com.guessaword.model;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class PlayerTest {
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void getPlayerId() {
+    }
+
+    @Test
+    public void setPlayerId() {
+    }
+
+    @Test
+    public void setStats() {
+    }
+
+    @Test
+    public void getStats() {
+    }
+
+    @Test
+    public void testToString() {
     }
 }

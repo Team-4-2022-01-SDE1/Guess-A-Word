8cae06cbe623c014a4add3c4d372c259d73d64eb
Leonard Box <lfbox7@yahoo.com>
2022-02-24 13:56:22 -0600

    test updates

--- test/com/guessaword/model/PlayerTest.java
+++ test/com/guessaword/model/PlayerTest.java
@@ -1,12 +1,31 @@
 package com.guessaword.model;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class PlayerTest {
+    Player player;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        player = new Player("Jay");
+    }
+
+    @Test
+    public void testGetName_returnsTrue_stringMatchesGetNameReturn() {
+        assertEquals("Jay", player.getName());
+    }
+
+    @Test
+    public void testSetName_returnsTrue_stringSetNameAndCheckIfGetNameReturnsSame() {
+        player.setName("Jeff");
+        assertEquals("Jeff", player.getName());
+    }
+
+    @Test
+    public void testGetId_returnsTrue_testsFirstPlayerCreatedAndSetIdIncrementsValue() {
+        assertEquals(0, player.getPlayerId());
     }
 }
